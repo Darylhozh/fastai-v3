2549f87a8d78466a8b2159a17ec6d455da405c00
Darylhozh <58208269+Darylhozh@users.noreply.github.com>
2019-11-28 11:10:25 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'O~~~to be filled~~~O'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1flr2FGdhu75nalSK_sYXWFsXA1k2MQrr'
 export_file_name = 'export.pkl'
 
-classes = ['Porifera','Cnidaria','Platyhelminthes','Annelida','Mollusca','Arthropoda','Chordata']
+classes = ['mammal','cephalopod','crustacean','fish','reptile']
 path = Path(__file__).parent
 
 app = Starlette()
